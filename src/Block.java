ec5c6f08be46b0017ea0081f85685b6c4f91fb43
Peter McCarthy <pete@peters-mbp.home>
2018-02-03 17:13:38 +0000

    added nonce to calcul;ated hash

--- src/Block.java
+++ src/Block.java
@@ -23,6 +23,7 @@ public class Block {
         String calculatedHash = StringUtil.applySha256(
                 previousHash +
                 Long.toString(timeStamp) +
+                Integer.toString(nonce) +
                 data
         );
         return calculatedHash;
